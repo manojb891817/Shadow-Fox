5aae4329bbc5289b8092f965614c4ac1cede685c
Manoj B <167521782+manojb891817@users.noreply.github.com>
2025-08-22 10:12:32 +0530

    Delete PORTFOLIO directory

--- PORTFOLIO/p.js
+++
@@ -1,7 +0,0 @@
-const typed = new Typed(".typing-text", {
-  strings: ["Frontend Developer", "Web Developer", "Tech Enthusiast"],
-  typeSpeed: 100,
-  backSpeed: 50,
-  backDelay: 1000,
-  loop: true
-});
